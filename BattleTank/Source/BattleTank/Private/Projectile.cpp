1f114ae28cbd7872eb565e6faf5ea79e89ba08f9
lxd_workPC <lxd3808@163.com>
2017-12-09 17:17:17 +0800

    未完成抛射动作

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -9,15 +9,14 @@ AProjectile::AProjectile()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Projectile Movement"));
+	ProjectileMovement->bAutoActivate = false;
 }
 
 // Called when the game starts or when spawned
 void AProjectile::BeginPlay()
 {
 	Super::BeginPlay();
-
-	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Projectile Movement"));
-	ProjectileMovement->bAutoActivate = false;
 	
 }
 
@@ -26,5 +25,12 @@ void AProjectile::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AProjectile::LaunchProjectile(float Speed) {
+	auto Time = GetWorld()->GetTimeSeconds();
+	UE_LOG(LogTemp, Warning, TEXT("%f projectile fires at %f"), Time, Speed);
+	ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector* Speed);
+	ProjectileMovement->Activate();
 }
 
--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -43,16 +43,15 @@ void ATank::SetTurretReference(UTankTurret* TurretToset) {
 }
 
 void ATank::Fire() {
-	auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("%f called Fire()"), Time);
 	if (!Barrel) { return; }
 
 	// 在socket位置生成抛射体
-	GetWorld()->SpawnActor<AProjectile>(
+	auto Projectile = GetWorld()->SpawnActor<AProjectile>(
 		ProjectileBlueprint, 
 		Barrel->GetSocketLocation(FName("Projectile")),
 		Barrel->GetSocketRotation(FName("Projectile"))
 		);
+	Projectile->LaunchProjectile(LaunchSpeed);
 
 }
 
