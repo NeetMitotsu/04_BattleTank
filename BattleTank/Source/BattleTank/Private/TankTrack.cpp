09bf92cabfeb921cdefa6e38ef17fd31be254072
lxd_workPC <lxd3808@163.com>
2017-12-13 18:03:58 +0800

    148未完， 不能在本体施加力

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+
+
+void UTankTrack::SetThrottle(float Throttle) {
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle : %f"), *Name, Throttle);
+
+	// TODO clamp actual throttle value so player can't over-drive
+	// TODO 控制实际的油门， 不能超速
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	UE_LOG(LogTemp, Warning, TEXT("%s ForceApplied : %f"), *ForceApplied.ToString(), Throttle);
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+	
+}
+
---
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * TankTrack is used to set maximum driving force, and to apply forces to the tank
+ */
+UCLASS(meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	// Set a throttle between -1 and +1 
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+	
+	// Max force per track , in Newtons
+	// 每个履带最大的力， 单位：N
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000; // Assume 40 tonne tank, and 1g accelleration 40吨坦克， 1g加速度
+	
+};
