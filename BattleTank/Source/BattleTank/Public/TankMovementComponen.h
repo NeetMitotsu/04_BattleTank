2860a07f070f5f63a0ece01ed987eacba68c90f4
NeetMitotsu <lxd3808@gmail.com>
2017-12-17 21:08:14 +0800

    151

---
+++ BattleTank/Source/BattleTank/Public/TankMovementComponen.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/NavMovementComponent.h"
+#include "TankMovementComponen.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API UTankMovementComponen : public UNavMovementComponent
+{
+	GENERATED_BODY()
+
+		void IntendMovForward(float Throw);
+	
+	
+};
